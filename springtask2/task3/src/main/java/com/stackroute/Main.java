d42aee24b231569bc9c499ef00febd8c704e13dc
satvikadarsh.kumar <satvikadarsh.kumar@cgi.com>
2019-09-26 11:32:35 +0530

    task6

--- springtask2/task3/src/main/java/com/stackroute/Main.java
+++ springtask2/task3/src/main/java/com/stackroute/Main.java
@@ -1,5 +1,6 @@
 package com.stackroute;
 import com.stackroute.demo.BeanLifecycleDemoBean;
+import com.stackroute.demo.BeanPostProcessorDemo;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -7,8 +8,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) throws Exception {
         AbstractApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        BeanLifecycleDemoBean demoBean = context.getBean("lifecycledemo", BeanLifecycleDemoBean.class);
-        System.out.println("LifeCycle Demo");
+        BeanPostProcessorDemo processordemo = context.getBean("processor", BeanPostProcessorDemo.class);
+        System.out.println("PostProcessor Demo");
         context.close();
     }
 }
