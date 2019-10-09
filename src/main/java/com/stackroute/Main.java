41d3bf9c157b10aac1fe1ed26e00a0dbc349e302
satvikadarsh.kumar <satvikadarsh.kumar@cgi.com>
2019-10-10 00:55:01 +0530

    con-inj

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -9,10 +9,10 @@ public class Main {
         {
             ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
             Movie SOTY3 = context.getBean("movie1", Movie.class);
-            SOTY3.callingactor();
+            SOTY3.toString();
 
             Movie DDLJ2 = context.getBean("movie2", Movie.class);
-            DDLJ2.callingactor();
+            DDLJ2.toString();
 
             System.out.println(SOTY3==DDLJ2);
         }
